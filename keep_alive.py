e9777c779faf657dba290bde73b80cfb70de57ed
suminb99 <suminbaek99@gmail.com>
2025-09-21 09:46:44 +0900

    feat: Flask 기반 keep_alive 웹 서버 추가

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from threading import Thread    
+
+app = (__name__)
+
+@app.route('/')
+def index():
+    return "봇 상태: 온라인"
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
