5f2753ebf14d009bbbd8a271957f06dc7f7bdb1e
suminb99 <suminbaek99@gmail.com>
2025-09-21 09:51:31 +0900

    feat: keep_alive 함수 호출

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import discord
 from discord.ext import commands
 import logging
-from dotenv import load_dotenv
 import os
 from datetime import datetime, timezone, timedelta
+from keep_alive import keep_alive
 
-load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+keep_alive()
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+token = os.environ['DISCORD_TOKEN']
+
+handler = logging.FileHandler(filename='discord.log',
+                              encoding='utf-8',
+                              mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
@@ -16,37 +19,42 @@ intents.reactions = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+
 @bot.event
 async def on_ready():
-    print(f"We are ready to go in, {bot.user.name}")
+    print("봇이 성공적으로 로그인했습니다!")
 
 
 # 벌금 메세지 수집
 async def get_fine_messages(channel):
     messages = []
-    delta = (datetime.now(timezone.utc) - timedelta(days=14)) # 2주 전 날짜 구하기
-    
+    delta = (datetime.now(timezone.utc) - timedelta(days=14))  # 2주 전 날짜 구하기
+
     async for message in channel.history(limit=50):
         if message.created_at < delta:
             break
 
-        if not (message.author.bot) and message.mentions:  # 봇이 작성하지 않은 멘션이 있는 메시지만 수집
+        if not (message.author.bot
+                ) and message.mentions:  # 봇이 작성하지 않은 멘션이 있는 메시지만 수집
             messages.append(message)
 
     return messages
-    
+
+
 # 체크 안 한 부원 판독
 async def get_unchecked_members(message):
-    mentioned = message.mentions # 멘션된 부원
-    checked = [] 
-    unchecked = [] 
-    reaction = discord.utils.get(message.reactions, emoji="✅") # 체크 이모지 객체 가져오기
+    mentioned = message.mentions  # 멘션된 부원
+    checked = []
+    unchecked = []
+    reaction = discord.utils.get(message.reactions,
+                                 emoji="✅")  # 체크 이모지 객체 가져오기
     if reaction:
         # 체크 한 부원
         async for member in reaction.users():
             checked.append(member.id)
-    
-    unchecked = [member for member in mentioned if member.id not in checked] # 체크 안 한 부원
+
+    unchecked = [member for member in mentioned
+                 if member.id not in checked]  # 체크 안 한 부원
 
     return unchecked
 
@@ -58,16 +66,13 @@ async def 벌금체크(ctx):
 
     for message in messages:
         unchecked = await get_unchecked_members(message)
-        all_unchecked.extend(unchecked) # 체크 안 한 부원들 모음
+        all_unchecked.extend(unchecked)  # 체크 안 한 부원들 모음
 
     if all_unchecked:
         mentions = " ".join(f"<@{member.id}>" for member in all_unchecked)
-        await ctx.send(
-            f"아직 벌금 안 내신 분들은 \n{mentions}\n입금 후 체크 표시(✅) 꼭 남겨주세요! "
-        )
+        await ctx.send(f"{mentions}\n이번 주 벌금 납부 안 하신 분들은 입금 후 체크 표시(✅) 꼭 남겨주세요!")
     else:
         await ctx.send("✅ 전원 입금 완료!")
 
 
 bot.run(token=token, log_handler=handler, log_level=logging.DEBUG)
-
